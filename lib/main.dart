28356ea5083c35300451b491d6aa51937353de1f
Arnau Gómez <agomez@mrmilu.com>
2021-06-27 07:23:17 +0200

    Added firebase notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,35 @@
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:joves_lectors/webview_container.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseMessaging.instance.getInitialMessage();
+
+    FirebaseMessaging.onMessage.listen((message) {
+      if(message.notification != null) {
+        print(message.notification.body);
+        print(message.notification.title);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,4 +37,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
